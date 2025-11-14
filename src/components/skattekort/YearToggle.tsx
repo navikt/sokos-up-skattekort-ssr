acdf3fd429e16fc6cfe503201d3895277b416df0
Jonas Nicolaysen <jonas.nicolaysen@nav.no>
2025-11-14 17:28:39 +0100

    fiks søking på år

--- src/components/skattekort/YearToggle.tsx
+++ src/components/skattekort/YearToggle.tsx
@@ -7,11 +7,28 @@ interface YearToggleProps {
 }
 
 export default function YearToggle({ years, defaultYear }: YearToggleProps) {
+  const handleYearChange = (value: string) => {
+    const yearInput = document.getElementById("year-input") as HTMLInputElement;
+    const form = document.getElementById("skattekort-form") as HTMLFormElement;
+    const fnrInput = form?.querySelector(
+      'input[name="fnr"]',
+    ) as HTMLInputElement;
+
+    if (yearInput && form) {
+      yearInput.value = value;
+
+      const fnrValue = fnrInput?.value.replace(/[\s.]/g, "");
+      if (fnrValue && fnrValue.length === 11 && /^[0-9]{11}$/.test(fnrValue)) {
+        form.requestSubmit();
+      }
+    }
+  };
+
   return (
     <ToggleGroup
       defaultValue={String(defaultYear)}
       size="small"
-      onChange={() => {}}
+      onChange={handleYearChange}
     >
       {years.map((year) => (
         <ToggleGroup.Item key={year} value={String(year)}>
