ea45e9c899b1079bdad051bf53d98b1bc3f2ccfc
mudasar187 <mudasar_cheema@hotmail.com>
2026-02-05 12:32:50 +0100

    fix versjon avhengighet for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,41 +5,43 @@ import prefixer from "postcss-prefix-selector";
 
 // https://astro.build/config
 export default defineConfig({
-  base: "/skattekort-ssr",
-  build: {
-    inlineStylesheets: "always",
-  },
-  vite: {
-    css: {
-      postcss: {
-        plugins: [
-          prefixer({
-            prefix: ".sokos-up-skattekort-ssr", // brukes for å unngå å lekke css ut av mikrofrontenden
-            ignoreFiles: [/module.css/],
-          }),
-        ],
-      },
-    },
-  },
-  integrations: [
-    react(),
-    {
-      name: "importmap-externals",
-      hooks: {
-        "astro:build:setup": ({ vite, target }) => {
-          if (target === "client") {
-            vite.build.rollupOptions.external = [
-              "react",
-              "react-dom",
-              "jsx-runtime",
-            ];
-          }
-        },
-      },
-    },
-  ],
-  output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+	base: "/skattekort-ssr",
+	build: {
+		inlineStylesheets: "always",
+	},
+	vite: {
+		css: {
+			postcss: {
+				plugins: [
+					prefixer({
+						prefix: ".sokos-up-skattekort-ssr", // brukes for å unngå å lekke css ut av mikrofrontenden
+						ignoreFiles: [/module.css/],
+					}),
+				],
+			},
+		},
+	},
+	integrations: [
+		react(),
+		{
+			name: "importmap-externals",
+			hooks: {
+				"astro:build:setup": ({ vite, target }) => {
+					if (target === "client") {
+						vite.build.rollupOptions.external = [
+							"react",
+							"react/jsx-runtime",
+							"react-dom",
+							"react-dom/client",
+							"scheduler",
+						];
+					}
+				},
+			},
+		},
+	],
+	output: "server",
+	adapter: node({
+		mode: "standalone",
+	}),
 });
